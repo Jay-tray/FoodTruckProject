0635dafcd163a379c82341315ba3d5f6fd84cb38
Jay-tray <113144309+Jay-tray@users.noreply.github.com>
2022-10-28 16:24:51 -0600

    initial commit

---
+++ src/com/skilldistillery/foodtruck/app/FoodTruckApp.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.foodtruck.app;
+
+public class FoodTruckApp {
+
+	public static void main(String[] args) {
+	///only static method in code^
+
+	}
+
+}
+
+
+
+/// menus, scanner, 
---
+++ src/com/skilldistillery/foodtruck/entities/FoodTruck.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.foodtruck.entities;
+
+public class FoodTruck {
+	private static int nextTruckID;
+	private String name;
+	
+	///NO MAIN
+}
